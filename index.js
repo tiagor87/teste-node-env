20656a3a4eb6b067c8c1ff5a5b279582f10511bf
Tiago de Carvalho Resende <tiago.dsn.erp@alterdata.matriz>
2017-08-25 16:32:08 -0300

    Add mongo connection

--- index.js
+++ index.js
@@ -8,26 +8,28 @@ const db = mongoose.createConnection(
     .DB_NAME}`
 );
 
+const itemSchema = new mongoose.Schema({
+  product: String,
+  quantity: Number,
+  value: Number
+});
+
+const Item = db.model('Item', itemSchema, 'items');
+
 const documentSchema = new mongoose.Schema({
   operation: { type: String, enum: ['in', 'out'] },
-  items: [
-    {
-      product: String,
-      quantity: Number,
-      value: Number
-    }
-  ]
+  items: [{ type: 'Item', lazy: true }]
 });
 
 const Document = db.model('Document', documentSchema, 'documents');
 let document = new Document();
 document.operation = 'out';
-document.items.push({
+const item = new Item({
   product: 'product',
   quantity: 1,
   value: 100
 });
-
+document.items.push(item);
 return document
   .save()
   .then(response => console.log(response))
